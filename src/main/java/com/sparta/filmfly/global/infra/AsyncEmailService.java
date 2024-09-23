e6612642912f2ce2d1748f9153182c89affda28b
원지연 <dnjs229@gmail.com>
2024-09-23 22:36:11 +0900

    [BUGFIX] 이메일 비동기 전송 에러 수정 5

--- src/main/java/com/sparta/filmfly/global/infra/AsyncEmailService.java
+++ src/main/java/com/sparta/filmfly/global/infra/AsyncEmailService.java
@@ -3,6 +3,7 @@ package com.sparta.filmfly.global.infra;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -15,6 +16,9 @@ public class AsyncEmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     @Async
     public CompletableFuture<Boolean> sendEmail(String to, String subject, String text) {
         try {
@@ -22,6 +26,7 @@ public class AsyncEmailService {
             msg.setTo(to);
             msg.setSubject(subject);
             msg.setText(text);
+            msg.setFrom(fromEmail);
 
             mailSender.send(msg);
             return CompletableFuture.completedFuture(true);
